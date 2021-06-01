c210c464901afb9054aa308863bedd6e46141a1e
러리 <doralife12@naver.com>
2021-06-01 19:15:35 +0900

    Feat: Basic node server

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var todayVisited = 0;
+
+http.createServer(function(request, response) {
+  console.log("request ", request.url);
+
+  if(request.url.startsWith("/counter")) {
+    todayVisited += 1;
+    response.writeHead(200, { "Content-Type" : "application/json" });
+    response.end(JSON.stringify({todayVisited}), "utf-8");
+  } else {
+    response.writeHead(200, { "Content-Type" : "text/plain" });
+    response.end(`Hello! You've requested with url ${request.url}!`, "utf-8");
+  }
+}).listen(8125);
+console.log("Serverr running at http://127.0.0.1:8125/");
